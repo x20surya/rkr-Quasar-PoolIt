6443da1bfe7962db3c36b2d271c3fd1b2331edb7
x20surya <suryanshuc659@gmail.com>
2024-04-19 19:15:58 +0530

    Update userProfile

--- src/app/(main)/(userProfile)/_layout.tsx
+++ src/app/(main)/(userProfile)/_layout.tsx
@@ -6,7 +6,9 @@ export default function AuthLayout (){
     return(
         <Stack>
             <Stack.Screen name="userProfile" options={{ headerShown: false }} />
-            
+            <Stack.Screen name="savedLocations" options={{ headerShown: false }} />
+            <Stack.Screen name="previousRides" options={{ headerShown: false }} />
+            <Stack.Screen name="about" options={{ headerShown: false }} />
         </Stack>
     )
 }
